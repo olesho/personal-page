19567aaae1caa9fabc2de011bb16395508ebd0cb
Oleh <olehluchkiv@MacBook-Pro-Nataliia.local>
2025-04-14 11:21:53 -0500

    worker added

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ module.exports = {
     app: './js/app.js',
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'personal-page/dist'),
     clean: true,
     filename: './js/app.js',
   },
