70c67f06a4f36aa0b5084e355665e29bd0fae884
Oleh <olehluchkiv@MacBook-Pro-Nataliia.local>
2025-04-14 11:59:37 -0500

    static

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ module.exports = {
     app: './js/app.js',
   },
   output: {
-    path: path.resolve(__dirname, 'personal-page/dist'),
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
     filename: './js/app.js',
   },
